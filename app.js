49a31905566056a76192dfedb5d99116b82fc6ed
Kidchris <christianjosaphat0@gmail.com>
2021-07-01 04:37:49 +0000

    last commit

--- app.js
+++ app.js
@@ -1,48 +1,11 @@
 const numbers = Array.from(document.querySelectorAll(".number"));
-const operators = Array.from(document.querySelectorAll(".operator"));
+// const operators = Array.from(document.querySelectorAll(".operator"));
 const output = document.getElementById("output");
 const display = document.getElementById("display");
 const del = document.getElementById("del");
 const clear = document.querySelector(".clear");
 //////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
-
-/////////////////////////////////////////////////////////////////////
-class Calculate {
-  addNumber(a, b) {
-    return a + b;
-  }
-
-  subNumber(a, b) {
-    return a - b;
-  }
-
-  divNumber(a, b) {
-    return a / b;
-  }
-
-  multNumber(a, b) {
-    return a * b;
-  }
-}
-
-function compute(operator, a, b) {
-  a = Number(a);
-  b = Number(b);
-  switch (operator) {
-    case "+":
-      return calculate.addNumber(a, b);
-    case "−":
-      return calculate.subNumber(a, b);
-    case "×":
-      return calculate.multNumber(a, b);
-    case "÷":
-      if (b === 0) return null;
-      else return calculate.divNumber(a, b);
-    default:
-      return null;
-  }
-}
 
 
 class Features {
@@ -66,24 +29,12 @@ const features = new Features();
 del.onclick = features.deleteNumber;
 clear.onclick = features.clearAll;
 
-let calculate = new Calculate();
 
 numbers.forEach((number) => {
   number.onclick = function () {
     if ((number.value === ".") & display.textContent.includes(".")) return;
     display.innerHTML += number.value;
+    output.innerHTML = eval(display.textContent)
   };
 });
 
-operators.forEach((operator) => {
-  operator.onclick = function () {
-    x = display.textContent;
-    // output.innerHTML = x;
-    display.innerHTML = "";
-    if (operator.value === "") return;
-    if (operator.value !== "" && display.textContent !=="") {
-      c = compute(operator.value, display.textContent, x)
-      output.innerHTML = c;
-    }
-  };
-});
